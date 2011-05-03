9eea12f918c19dc86a4051fc290cf4113305d98c
Chuck Remes <cremes@mac.com>
2011-05-03 11:21:13 -0500

    specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,19 @@
 require File.expand_path(
     File.join(File.dirname(__FILE__), %w[.. lib zmqmachine]))
 
-Spec::Runner.configure do |config|
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-end
+#Rspec.configure do |config|
+#  # == Mock Framework
+#  #
+#  # RSpec uses it's own mocking framework by default. If you prefer to
+#  # use mocha, flexmock or RR, uncomment the appropriate line:
+#  #
+#  # config.mock_with :mocha
+#  # config.mock_with :flexmock
+#  # config.mock_with :rr
+#end
 
+class FakeHandler
+  def on_attach socket
+    
+  end
+end
